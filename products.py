e8561d03cfed09db02e5484b5007aa0f788d57cc
Daniel Huang <dnlhuang@gmail.com>
2018-08-14 22:19:51 +0800

    add check file

--- products.py
+++ products.py
@@ -1,19 +1,25 @@
-# 讀取檔案
-products = []
-with open('products.csv', 'r', encoding = 'utf-8') as f:
-    for line in f:
-        if '商品,價格' in line:
-            continue  # continue跟break一樣只能寫在迴圈(第5~11行)裡。 continue就是跳到下一迴的意思
-        name, price = line.strip().split(',')
-        # name = s[0]   price = s[1]
-        # strip 去除 \n 還有 前後的空格
-        # split 切割，()裡是切割的標記，切割完是清單[]
-        products.append([name, price])
-print(products)
+# 作業系統模組（第五步驟）
+import os # operating system，標準函式庫中
+# path 路徑； isfile 檢查檔案在不在
 
+products = [] #先產生空清單
+if os.path.isfile('products.csv'): # 檢查'products.csv'是否與本程式('product.py')在同一資料夾中
+    print('找到檔案了！')
+    with open('products.csv', 'r', encoding = 'utf-8') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue  # continue跟break一樣只能寫在迴圈(第5~11行)裡。 continue就是跳到下一迴的意思
+            name, price = line.strip().split(',')
+            # name = s[0]   price = s[1]
+            # strip 去除 \n 還有 前後的空格
+            # split 切割，()裡是切割的標記，切割完是清單[]
+            products.append([name, price])
+    print(products)
 
+else:
+    print('沒找到檔案！')
 
-
+# 讓使用者輸入（第一步驟）
 # 重複輸入：所以用到「迴圈」
 # while True：適用於不知道會重複幾次的迴圈
 # 兩個維度的清單，清單中還有清單
@@ -27,9 +33,11 @@ while True:
     products.append([name, price])  
 print(products)
 
+# 印出所有商品紀錄 （第二步驟）
 for p in products:
     print(p[0], '的價格是', p[1])
 
+# 寫入檔案（第三步驟）
 # 'abc' + 'abc' = 'abcabc'
 # 'abc' * 3 = 'abcabcabc'
 # open 打開檔案
